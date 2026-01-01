a73ce2a85226dfec08df5edaaa93117b27067d98
37723 <37723@iqraisb.edu.pk>
2026-01-01 13:45:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the current date
+const today = new Date();
+
+// Extract the current year
+const currentYear = today.getFullYear();
+
+// Display the year in the footer
+document.getElementById("currentYear").textContent = currentYear;
